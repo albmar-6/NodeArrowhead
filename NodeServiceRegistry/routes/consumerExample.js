94565089b5882e9216d4a106c18f9532b280ad87
albmar-6 <albin.martinsson92@gmail.com>
2018-12-16 13:46:23 +0100

    Update consumerExample.js

--- NodeServiceRegistry/routes/consumerExample.js
+++ NodeServiceRegistry/routes/consumerExample.js
@@ -1,12 +1,10 @@
-var serviceregistry = require('./serviceregistry');
 var WebSocket = require('ws');
 
-//Parse and port adress from db.
+var wss = new WebSocket.Server({port: 3004});
 
-var ws = new WebSocket('ws://localhost:3004');
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+      console.log(message);
+  });
+});
 
-ws.on('open', function open(){
-  ws.send('tempC')
-})
-
-console.log(serviceregistry.getProvider("getTemp-01"));
